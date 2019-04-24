668becc40cd2b7089da1a7dc7a54b1af766bf320
Andrey Kalinin <prize2step@gmail.com>
2019-04-24 15:23:29 +0300

    GG-14970 IdleVerify command should print end time of execution.

--- modules/core/src/test/java/org/apache/ignite/util/GridCommandHandlerTest.java
+++ modules/core/src/test/java/org/apache/ignite/util/GridCommandHandlerTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 GridGain Systems, Inc. and Contributors.
- * 
+ *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -2480,8 +2480,10 @@ public class GridCommandHandlerTest extends GridCommonAbstractTest {
 
         assertTrue(lastRowIndex > 0);
 
-        // Last row is empty, but the previous line contains data
-        lastRowIndex = log.lastIndexOf('\n', lastRowIndex - 1);
+        for (int i = 0; i < 2; i++) {
+            // Last row is empty, but the previous line contains data
+            lastRowIndex = log.lastIndexOf('\n', lastRowIndex - 1);
+        }
 
         assertTrue(lastRowIndex > 0);
 
